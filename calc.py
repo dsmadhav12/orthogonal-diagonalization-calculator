bbe3b6b7b551e0ec328dd9a702388a8a5bf6da26
Madhav Donepudi <dsmadhav@hotmail.com>
2024-11-27 01:04:54 -0500

    Update calc.py to check if A is ortho_diagonalizable

--- calc.py
+++ calc.py
@@ -1,36 +1,61 @@
 import numpy as np
-
+from sympy import Matrix
 def input_matrix(size):
     print(f"Enter the elements of {size}x{size} matrix row by row:")
     matrix = []
     for i in range(size):
         row = []
         for j in range(size):
-            value = float(input(f"Enter value for element [{i+1}][{j+1}]: "))
+            value = int(input(f"Enter value for element [{i+1}][{j+1}]: "))
             row.append(value)
         matrix.append(row)
     return matrix
+
 
 def print_matrix(matrix):
     for i in matrix:
         print(i)
     print()
-def diagonalize(matrix,size):
-    numpyMatrix=np.array(matrix)
-    eigenvalues, eigenvectors = np.linalg.eig(numpyMatrix)
     
-    if(eigenvectors.shape[1] ==size):
-        D = np.diag(eigenvalues)
-        P = eigenvectors
-        print("P: " +str(P))
+def diagonalize(matrix, size):
+    matrix = Matrix(matrix)
+    lin_inp =find_eigenspaces(matrix)
+    if(lin_inp == size):
+        P, D = matrix.diagonalize()
+        P=P.tolist()
+        D=D.tolist()
+        print("A is diagonalizeable")
         print()
-        print("D: " +str(D)) 
+        print("P = ")
+        print_matrix(P)
+        print()
+        print("D = ") 
+        print_matrix(D)
         return P, D
     else:
-        print("The matrix is niot diagnolizable")
-        return None, None
+        print("The matrix is not diagonalizable")
+        return None
+
+def find_eigenspaces(matrix):
+    tempMatrix = Matrix(matrix)
+    eigenvectors = tempMatrix.eigenvects()
+    total_independent_eigenvectors = sum(len(v[2]) for v in eigenvectors)
+    
+    return total_independent_eigenvectors
+
+def orth_diagonalizable(matrix):
+    matrix = Matrix(matrix)
+    matrixTranspose = matrix.T
+    matrixTranspose = matrixTranspose.tolist()
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i,j] != matrixTranspose[i][j]:
+                return False
+    return True
     
 
+def Gram_Schmidt(matrix, size):
+    print("finish tmr")
 def main():
     while True:
         try:
@@ -43,8 +68,17 @@ def main():
             print("Invalid input. Please enter a number (2 or 3).")
     
     matrix = input_matrix(size)
+    print("A = ")
     print_matrix(matrix)
     diagonalize(matrix, size)
+    if(orth_diagonalizable(matrix) == False):
+        print("A is not orthogonally diagonalizable")
+    else:
+        print("A is orthogonally diagonalizable")
+        print()
+
+    
+
 
 if __name__ == "__main__":
     main()
